df714eb09ef7f66dfe07438c90c5b0dbfc003eb9
dengxinwei <dengxinwei@yy.com>
2019-06-06 19:07:58 +0800

    刷题 215

--- src/main/java/com/interview/array/leetcode_215/Solution.java
+++ src/main/java/com/interview/array/leetcode_215/Solution.java
@@ -1,5 +1,17 @@
 package com.interview.array.leetcode_215;
 
-public class Solution {
+import java.util.PriorityQueue;
 
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue q = new PriorityQueue();
+        for (int i = 0; i < nums.length; i++) {
+            q.offer(nums[i]);
+            if(q.size()>k){
+                q.poll();
+            }
+        }
+
+        return (int)q.poll();
+    }
 }
