648713a26b61818a31fca1861e8861c6826e4ea6
yanisdxw <yanisdxw@gmail.com>
2019-06-28 00:00:43 +0800

    递归生成有效括号

---
+++ src/main/java/com/interview/others/Parenthesis/leetcode_22.java
@@ -0,0 +1,26 @@
+package com.interview.others.Parenthesis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode_22 {
+
+    private List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        gen(n,0,0,"");
+        return res;
+    }
+
+    private void gen(int n, int left, int right, String result){
+        if(left==n && right==n) {
+            res.add(result);
+        }
+        if(left<n){
+            gen(n,left+1,right,result+"(");
+        }
+        if(right<n && right<left){
+            gen(n,left,right+1,result+")");
+        }
+    }
+}
