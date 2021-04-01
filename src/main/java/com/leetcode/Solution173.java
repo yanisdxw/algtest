7d7e184ae067502a8c6a990bbc1bf24c7be083f2
dengxw <dengxw@rd.netease.com>
2021-04-01 13:18:44 +0800

    最近做的题：dfs，treenode

---
+++ src/main/java/com/leetcode/Solution173.java
@@ -0,0 +1,69 @@
+package com.leetcode;
+
+import com.leetcode.po.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 173. 二叉搜索树迭代器
+ *
+ */
+public class Solution173 {
+
+    //使用一个数据存储二叉搜索树的先序遍历结果
+    //不过这种需要用o(n)的空间
+//    class BSTIterator {
+//        List<Integer> nodesArray = new ArrayList<>();
+//        Iterator<Integer> iterator;
+//        public BSTIterator(TreeNode root) {
+//            //先中序遍历二叉搜索树
+//            if(root==null) return;
+//            inOrder(root);
+//            iterator = nodesArray.listIterator();
+//        }
+//        public void inOrder(TreeNode node){
+//            if(node==null) return;
+//            inOrder(node.left);
+//            nodesArray.add(node.val);
+//            inOrder(node.right);
+//        }
+//        public int next() {
+//            return iterator.next();
+//        }
+//        public boolean hasNext() {
+//            return iterator.hasNext();
+//        }
+//    }
+
+    //优化：使用一个栈存储当前node，遍历完就出栈
+    //空间复杂度n(h)
+    class BSTIterator {
+        Stack<TreeNode> stack = new Stack<>();
+
+        //先存储一部分节点到栈中
+        private void treeNodePush(TreeNode node){
+            while (node!=null){
+                stack.push(node);
+                node = node.left;
+            }
+        }
+
+        public BSTIterator(TreeNode root) {
+            //先中序遍历二叉搜索树
+            if(root==null) return;
+            treeNodePush(root);
+        }
+
+        public int next() {
+            TreeNode top = stack.pop();
+            treeNodePush(top.right);
+            return top.val;
+        }
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+    }
+}
---
+++ src/main/java/com/leetcode/Solution190.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+/**
+ * 190. 颠倒二进制位
+ * 颠倒给定的 32 位无符号整数的二进制位。
+ */
+public class Solution190 {
+    public int reverseBits(int n) {
+        int ans = 0;//总数
+        for (int i = 0; i < 32; i++) {
+            ans<<=1;
+            ans+=n&1;
+            n>>=1;
+        }
+        return ans;
+    }
+}
